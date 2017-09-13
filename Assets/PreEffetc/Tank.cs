b40059db616b9e9839f47d407bae661cd5d6ba2e
Nisshii <nkc.daiki.nishiwaki@gmail.com>
2017-09-13 12:21:47 +0900

    自機(戦車)の追加　色付き砲弾の作成　パーティクルシステムの使用、作成

---
+++ Assets/PreEffetc/Tank.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class Tank : MonoBehaviour {
+
+    public float rotate;
+
+	// Use this for initialization
+	void Start () {
+		
+	}
+	
+	// Update is called once per frame
+	void Update () {
+        transform.Rotate(new Vector3(rotate, 0, 0));
+		
+	}
+}
