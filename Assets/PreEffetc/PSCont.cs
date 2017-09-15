aa9247cb97a034e5c7ef17db13293d2012e8bca9
Nisshii <nkc.daiki.nishiwaki@gmail.com>
2017-09-15 10:01:03 +0900

    エフェクトの追加

---
+++ Assets/PreEffetc/PSCont.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PSCont : MonoBehaviour
+{
+    private ParticleSystem particle;
+    // Use this for initialization
+    void Start()
+    {
+        particle = GetComponent<ParticleSystem>();
+
+        particle.Stop();
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        if (Input.GetButton("Jump"))
+        {
+            particle.Play();
+        }
+    }
+}
--- Assets/PreEffetc/Tank.cs
+++ Assets/PreEffetc/Tank.cs
@@ -4,16 +4,52 @@ using UnityEngine;
 
 public class Tank : MonoBehaviour {
 
-    public float rotate;
+    public Animator animator;
 
-	// Use this for initialization
-	void Start () {
-		
+    Vector3 BarrelVec;
+    float Barrel_Back = -20f;
+    float Barrel_return = 2.0f;
+
+    bool Barrelflag = true;
+    int i;
+
+    // Use this for initialization
+    void Start () {
+        BarrelVec = transform.position;
 	}
 	
 	// Update is called once per frame
 	void Update () {
-        transform.Rotate(new Vector3(rotate, 0, 0));
-		
+        //animation
+        if (Input.GetKeyDown(KeyCode.UpArrow))
+        {
+            animator.SetBool("fire", true);
+        }
+        else
+        {
+            animator.SetBool("fire", false);
+        }
+        //program
+        if (Barrelflag = true && Input.GetKeyDown(KeyCode.DownArrow))
+        {
+            BarrelVec.z = Barrel_Back;
+            transform.position = BarrelVec;
+            Barrelflag = false;
+            Debug.Log("false");
+        }
+        if (Barrelflag == false)
+        {
+            for (i = 0; i <= 10; i++)
+            {
+                BarrelVec.z += Barrel_return;
+                transform.position = BarrelVec;
+            }
+            if (i >= 10)
+            {
+                Barrelflag = true;
+                i = 0;
+                Debug.Log("true");
+            }
+        }
 	}
 }
