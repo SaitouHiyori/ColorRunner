ebae75169f4b06a2f47674699e414ec262f8ef8e
DESKTOP-EI6JT1O\user <nkc.ryo.shiratori@gmail.com>
2017-10-03 11:40:17 +0900

    タイトル　リザルト　統合（少しいじったやつ）

---
+++ Assets/Scripts/MoveTank.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MoveTank : MonoBehaviour {
+    public float Speed; //スクロール速度
+
+    //ランダム位置生成上限・下限
+    public float HeighEnd; //上限
+    public float LowEnd;   //下限
+
+    public float ScrollStart; //スクロール開始地点
+    public float ScrollEnd;   //スクロール終了地点
+
+    private void Update()
+    {
+        //スクロール
+        Vector3 MovePos = new Vector3(Speed, 0, 0) * Time.deltaTime;
+        transform.position += MovePos;
+
+        //スクロール終了地点を過ぎたら開始地点に戻す
+        if (transform.position.x < ScrollStart)
+        {
+            float yPos = Random.Range(LowEnd, HeighEnd);
+            Vector3 ReturnPos = new Vector3(ScrollStart, yPos, 1);
+            transform.position = ReturnPos;
+        }
+    }
+
+}
